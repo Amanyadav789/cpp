603070fd160e6b0c065564a454fb9a4c8150a010
Amanyadav789 <yadavaman8511005211@gmail.com>
2023-12-06 14:07:10 +0530

    first commit

---
+++ expenses.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     #ifndef ONLINE_JUDGE //not part of code, used to beautify input output
+              freopen("input.txt","r", stdin);
+              freopen("output.txt","r", stdout);
+     #endif
+              
+
+
+
+
+
+       return 0;
+
+}
