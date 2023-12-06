511ab56b64a4bfc8dceb2ddd842420d8dd6175ad
Amanyadav789 <yadavaman8511005211@gmail.com>
2023-12-06 16:12:34 +0530

    first commit

---
+++ switch.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    char button;
+    cout<<"input a character: ";
+    cin>>button;
+
+    if (button=='a')
+    {
+        cout<<"Hello"<<endl;
+    }
+
+    else if (button=='b')
+    {
+        cout<<"Namaste"<<endl;
+    }
+    else if (button=='c')
+    {
+        cout<<"salut"<<endl;
+    }
+    else if (button=='d')
+    {
+        cout<<"ciao"<<endl;
+    }
+    else if (button=='e')
+    {
+        cout<<"Hola"<<endl;
+    }
+    else{
+        cout<<"I am still learning more"<<endl;
+    }
+
+
+
+
+
+
+    return 0;
+}
